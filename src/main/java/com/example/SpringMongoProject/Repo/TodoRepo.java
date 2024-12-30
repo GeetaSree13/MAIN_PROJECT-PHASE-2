3e3a4c26b96dd91027a78b4655c1d5bab4612917
GeetaSree13 <117423480+GeetaSree13@users.noreply.github.com>
2024-12-30 09:49:53 +0530

    Update TodoRepo.java

--- src/main/java/com/example/SpringMongoProject/Repo/TodoRepo.java
+++ src/main/java/com/example/SpringMongoProject/Repo/TodoRepo.java
@@ -4,7 +4,7 @@ import com.example.SpringMongoProject.Entity.Todo;
 import com.example.SpringMongoProject.Entity.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface TodoRepo extends MongoRepository<Todo,String> {
