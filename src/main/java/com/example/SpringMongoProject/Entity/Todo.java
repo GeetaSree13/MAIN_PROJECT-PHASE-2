7c95aea00e49e0172c879265325909a1640017a4
GeetaSree13 <117423480+GeetaSree13@users.noreply.github.com>
2024-12-30 09:49:00 +0530

    Update Todo.java

--- src/main/java/com/example/SpringMongoProject/Entity/Todo.java
+++ src/main/java/com/example/SpringMongoProject/Entity/Todo.java
@@ -2,6 +2,7 @@ package com.example.SpringMongoProject.Entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "todos")
 public class Todo {
