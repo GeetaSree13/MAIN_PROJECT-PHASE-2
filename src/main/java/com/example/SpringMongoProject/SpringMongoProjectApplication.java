b340c65d49541e7e6197595376836601d26583ad
GeetaSree13 <117423480+GeetaSree13@users.noreply.github.com>
2024-12-30 09:50:57 +0530

    Update SpringMongoProjectApplication.java

--- src/main/java/com/example/SpringMongoProject/SpringMongoProjectApplication.java
+++ src/main/java/com/example/SpringMongoProject/SpringMongoProjectApplication.java
@@ -1,7 +1,7 @@
 package com.example.SpringMongoProject;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMongoProjectApplication {
